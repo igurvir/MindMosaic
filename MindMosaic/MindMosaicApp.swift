42c73dc1863dfa9cace27d8006d640518460d3af
igurvir <gurvirsingh0504@gmail.com>
2024-10-28 01:32:05 -0400

    Initial Commit

---
+++ MindMosaic/MindMosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMosaicApp.swift
+//  MindMosaic
+//
+//  Created by Gurvir Singh on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct MindMosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
